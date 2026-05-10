b358b2774f0a7b1b714c2118b1770ebe6c103d1f
Hasibul Hasan <hasibulh798@gmail.com>
2026-05-10 23:28:20 +0600

    fix: modify auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -169,16 +169,8 @@ export const auth = betterAuth({
         name: "session_token",
         attributes: {
           httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        },
-      },
-      state: {
-        name: "session_token",
-        attributes: {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         },
       },
     },
